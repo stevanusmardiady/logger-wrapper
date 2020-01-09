338f1a459fde481b472c4d6df3bfb65dbdfeb3b1
stevanus <stevanus.mardiady@infracom-tech.com>
2020-01-09 14:39:24 +0700

    edit

--- build-package.sh
+++ build-package.sh
@@ -1,4 +1,5 @@
 PROJECT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
+echo "---------------------------------------------"
 echo "Project dir is $PROJECT_DIR"
 
 echo "---------------------------------------------"
@@ -6,11 +7,15 @@ echo "Load build number & version number from file"
 export BUILD_NUMBER=$(cat build_conf/BUILD_NUMBER)
 export VERSION_NUMBER=$(cat build_conf/VERSION_NUMBER)
 export PACKAGE_NAME=$(cat build_conf/PACKAGE_NAME)
+echo "Build Number 		: $BUILD_NUMBER"
+echo "Version Number 	: $VERSION_NUMBER"
+echo "Package Name 		: $BUILD_NUMBER"
 
 echo "---------------------------------------------"
 echo "Increment the build number for each build"
 export BUILD_NUMBER=$(($BUILD_NUMBER+1))
 echo "$BUILD_NUMBER" > $PROJECT_DIR/build_conf/BUILD_NUMBER
+echo "(Inc) Build Number: $BUILD_NUMBER"
 
 echo "---------------------------------------------"
 echo "Trigger conda build process"
@@ -20,6 +25,8 @@ echo "---------------------------------------------"
 echo "Copy conda build artefact to private repo directory"
 export CONDA_BUILD_DIR=$(cat build_conf/CONDA_BUILD_DIR)
 export CONDA_REPO_DIR=$(cat build_conf/CONDA_REPO_DIR)
+echo "Conda Build Dir 	: $CONDA_BUILD_DIR"
+echo "Conda Repo Dir 	: $CONDA_REPO_DIR"
 
 echo "---------------------------------------------"
 echo "Copying all files under $CONDA_BUILD_DIR/$PACKAGE_NAME-$VERSION_NUMBER.$BUILD_NUMBER* into $CONDA_REPO_DIR"
