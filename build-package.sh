869912601aa6a483109edea5b186c6010d3c1d25
stevanus <stevanus.mardiady@infracom-tech.com>
2020-01-09 14:15:37 +0700

    edit

--- build-package.sh
+++ build-package.sh
@@ -1,25 +1,32 @@
 PROJECT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
 echo "Project dir is $PROJECT_DIR"
 
-# Load build number & version number from file
+echo "---------------------------------------------"
+echo "Load build number & version number from file"
 export BUILD_NUMBER=$(cat build_conf/BUILD_NUMBER)
 export VERSION_NUMBER=$(cat build_conf/VERSION_NUMBER)
 export PACKAGE_NAME=$(cat build_conf/PACKAGE_NAME)
 
-# Increment the build number for each build
+echo "---------------------------------------------"
+echo "Increment the build number for each build"
 export BUILD_NUMBER=$(($BUILD_NUMBER+1))
 echo "$BUILD_NUMBER" > $PROJECT_DIR/build_conf/BUILD_NUMBER
 
-# Trigger conda build process
+echo "---------------------------------------------"
+echo "Trigger conda build process"
 conda build $PROJECT_DIR
 
-# Copy conda build artefact to private repo directory
+echo "---------------------------------------------"
+echo "Copy conda build artefact to private repo directory"
 export CONDA_BUILD_DIR=$(cat build_conf/CONDA_BUILD_DIR)
 export CONDA_REPO_DIR=$(cat build_conf/CONDA_REPO_DIR)
+
+echo "---------------------------------------------"
 echo "Copying all files under $CONDA_BUILD_DIR/$PACKAGE_NAME-$VERSION_NUMBER.$BUILD_NUMBER* into $CONDA_REPO_DIR"
 cp $CONDA_BUILD_DIR/$PACKAGE_NAME-$VERSION_NUMBER.$BUILD_NUMBER* $CONDA_REPO_DIR
 
-# Perform conda index to update repo metadata
+echo "---------------------------------------------"
+echo "Perform conda index to update repo metadata"
 conda index $CONDA_REPO_DIR
 
 # Push build number back to git
